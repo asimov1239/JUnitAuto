73eeada79a872c2c1e1383e9b1d220830ac34593
Gabriel Bonfim Silva de Moraes <gabrielbonfim200699@gmail.com>
2023-07-19 08:57:29 -0300

    upgrade testselenium

--- src/test/java/org/example/Selenium2LearnTest.java
+++ src/test/java/org/example/Selenium2LearnTest.java
@@ -1,16 +1,40 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Selenium2LearnTest {
 
-    @Test
-    void testTitle(){
-        Selenium2Learn learning = new Selenium2Learn();
-        assertEquals("Nope Labs",learning.validateLogin());
+    ChromeDriver driver;
+
+    @BeforeEach
+    void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/v1/");
     }
 
 
+    @Test
+    void testTitle(){
+        Selenium2Learn learning = new Selenium2Learn();
+        assertEquals("Swag Labs",learning.validateLogin());
+    }
+
+    @Test
+    void testURL(){
+        assertEquals("https://www.saucedemo.com/v1/", driver.getCurrentUrl());
+    }
+
+//    @Test
+//    void testLogin(){
+//        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+//        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+//    }
+//
 }
